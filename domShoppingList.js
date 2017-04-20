cbcbfd6c41ac379da3f1496d3d7b2354889ada95
Evan Briolat <briolate@gmail.com>
2017-04-20 09:01:37 -0400

    First commit

---
+++ domShoppingList.js
@@ -0,0 +1,55 @@
+//consoleShoppingList
+// var container = document.createElement('div');
+// var container = document.createElement('div 2');
+// document.body.appendChild(container);
+
+// var header = document.createElement('h1');
+// document.appendChild()
+
+// name.className = "div1";
+// price.className = "div2");
+
+
+var groceryItems = [
+  {
+    name: 'Mango',
+    price: 2
+  },
+  {
+    name: 'Milk',
+    price: 3.99
+  },
+  {
+    name: 'Bread',
+    price: 4.99
+  },
+  {
+    name: 'Beer',
+    price: 12.99
+  },
+  {
+    name: 'Scotch',
+    price: 39.99
+  },
+  {
+    name: 'Dom Perignon',
+    price: 189.99
+  }
+];
+
+var total = 0;
+
+groceryItems.forEach(function(item) {
+var name = document.createElement('ol');
+name.innerText = item.name;
+var price = document.createElement('li');
+price.innerText = item.price;
+document.body.appendChild(name);
+document.body.appendChild(price);
+total += item.price;
+console.log(total);
+});
+
+var totalEle = document.createElement('p');
+document.body.appendChild(totalEle);
+totalEle.innerText = 'The total price is $' + total.toFixed(2);
